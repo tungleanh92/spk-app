f5fac2bde8ee4faae9f84cb87c50c66193b0e037
Sotatek-TungLe3 <tung.le3@sotatek.com>
2022-11-04 17:58:47 +0700

    done

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,14 @@
 use std::ops::{Mul, Sub};
 
 use chrono::{DateTime, Utc};
+use ed25519_dalek::Verifier;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LookupMap;
 use near_sdk::json_types::U128;
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{
-    env, ext_contract, log, near_bindgen, require, AccountId, Balance, BorshStorageKey, Gas,
-    PromiseError, Promise, PromiseOrValue, PromiseResult
+    bs58, env, ext_contract, near_bindgen, require, AccountId, Balance, BorshStorageKey, Gas,
+    Promise, PromiseError,
 };
 
 pub mod external;
@@ -83,6 +84,7 @@ pub struct Room {
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
+    pub owner: AccountId,
     pub token_address: AccountId,
     pub verified_amount: Balance,
     pub room_list: LookupMap<u128, Room>,
@@ -99,6 +101,7 @@ impl Contract {
     #[init]
     pub fn new(_verified_amount: Balance, _token_address: AccountId) -> Self {
         Contract {
+            owner: env::signer_account_id(),
             token_address: _token_address,
             verified_amount: _verified_amount,
             room_list: LookupMap::new(StorageKey::RoomIDKey),
@@ -114,11 +117,8 @@ impl Contract {
         _minutes_lasts: i64,
     ) {
         let _learner = env::signer_account_id();
-        
-        let _staked_amount = Self::query_staked_amount(&self, _advisor.clone());
-        env::promise_then(_staked_amount);
-        // require!(_staked_amount >= self.verified_amount, "App: Not an advisor!");
-        
+
+        Self::query_staked_amount(&self, _advisor.clone());
 
         let _pending_amount = _amount_per_minute.mul(_minutes_lasts as u128);
 
@@ -178,28 +178,46 @@ impl Contract {
             );
     }
 
-    pub fn end_room(&self, _room_id: u128, _learner_vote: u8) {
-        // signature?
-        require!(
-            self.room_list.contains_key(&_room_id) == true,
-            "App: Room not existed!"
-        );
-        let mut room = self.room_list.get(&_room_id).unwrap();
-        require!(room.claimed == false, "App: Already claimed!");
-        require!(room.reverted == false, "App: Already reverted!");
-        // require!(room.learner == recoverAddress, "App: Signature's learner not match");
+    pub fn end_room(&self, _room_id: u128, _learner_vote: u8, signature: Vec<u8>) {
+        // https://stackoverflow.com/questions/70041130/how-to-verify-secp256k1-signed-message-in-smart-contract
+        // verify signature of app creator
+        let signature = ed25519_dalek::Signature::try_from(signature.as_ref())
+            .expect("Signature should be a valid array of 64 bytes [13, 254, 123, ...]");
+        let public_key = ed25519_dalek::PublicKey::from_bytes(
+            &bs58::decode(
+                // private key of  app creator
+                "H5ANpdUoXVwhYBgAgEi1ieMQZKJbwxjPJtHX4vkVcSnF",
+            )
+            .into_vec()
+            .unwrap(),
+        )
+        .unwrap();
+        if let Ok(_) = public_key.verify(self.owner.as_bytes(), &signature) {
+            // succeed
+            require!(
+                self.room_list.contains_key(&_room_id) == true,
+                "App: Room not existed!"
+            );
+            let mut room = self.room_list.get(&_room_id).unwrap();
+            require!(room.claimed == false, "App: Already claimed!");
+            require!(room.reverted == false, "App: Already reverted!");
 
-        let minutes_last = Utc::now().timestamp().sub(room.start_time);
-        require!(
-            minutes_last >= room.minutes_last,
-            "App: Too early to reveive token!"
-        );
+            let minutes_last = Utc::now().timestamp().sub(room.start_time);
+            require!(
+                minutes_last >= room.minutes_last,
+                "App: Too early to reveive token!"
+            );
 
-        ext_ft_contract::ext(self.token_address.clone())
-            .with_static_gas(FT_TRANSFER_GAS)
-            .ft_transfer(room.advisor, U128::from(room.pending_amount), None);
+            // update interest in spk-stake
 
-        room.claimed = true;
+            ext_ft_contract::ext(self.token_address.clone())
+                .with_static_gas(FT_TRANSFER_GAS)
+                .ft_transfer(room.advisor, U128::from(room.pending_amount), None);
+
+            room.claimed = true;
+        }
+        // fail
+        require!(1 != 1, "There was an error verifying owner signature");
     }
 
     pub fn revert_token(&self, _room_id: u128) {
@@ -222,13 +240,12 @@ impl Contract {
 
     #[private]
     pub fn query_staked_amount(&self, _advisor_id: AccountId) -> Promise {
-        // Create a promise to call HelloNEAR.get_greeting()
         let promise = ext_stake_contract::ext(self.token_address.clone())
             .with_static_gas(Gas(5 * TGAS))
             .get_staked_amount(_advisor_id);
 
         return promise.then(
-            // Create a promise to callback query_greeting_callback
+            // Create a promise to callback query_staked_amount
             Self::ext(env::current_account_id())
                 .with_static_gas(Gas(5 * TGAS))
                 .query_staked_amount_callback(),
@@ -247,6 +264,7 @@ impl Contract {
 
         // Return the amount
         let amount = call_result.unwrap();
+        require!(amount >= self.verified_amount, "App: Not an advisor!");
         amount
     }
 }
